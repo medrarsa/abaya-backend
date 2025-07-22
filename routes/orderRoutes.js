bb7688ae5c4387594206657e7682f8698013ab6a
medrarsa <medrar.saudi@gmail.com>
2025-07-22 22:12:12 +0300

    Update orderRoutes.js

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -357,7 +357,7 @@ router.post('/bulk-delete', async (req, res) => {
   }
 });
 
- router.put('/:id', async (req, res) => {
+router.put('/:id', async (req, res) => {
   try {
     const order = await Order.findByIdAndUpdate(req.params.id, req.body, { new: true });
     if (!order) return res.status(404).json({ error: "الطلب غير موجود" });
@@ -366,6 +366,7 @@ router.post('/bulk-delete', async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 });
+
 
 
 
